b7e400031eb167655d830e60b5f709eae902a25c
Mark Repka <mark@repkam09.com>
2016-02-17 15:10:52 -0500

    Fix some code style problems

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
-var browserify = require('browserify');
-var gulp = require('gulp');
-var rename = require('gulp-rename');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var reactify = require('reactify');
+var browserify = require("browserify");
+var gulp = require("gulp");
+var rename = require("gulp-rename");
+var source = require("vinyl-source-stream");
+var buffer = require("vinyl-buffer");
+var reactify = require("reactify");
 
-var src = 'src/';
-var output = '../build/';
+var src = "src/";
+var output = "../build/";
 
 // Change into the src directory
 process.chdir(src);
 
 var createBrowserify = function () {
 	return browserify({
-		entries : './website-react.js',
-		extensions : ['.jsx'], // Needed to find the React JSX source files.
+		entries : "./website-react.js",
+		extensions : [".jsx"], // Needed to find the React JSX source files.
 		transform : [reactify],
 		cache : {}, // Required by watchify.
 		packageCache : {}, // Required by watchify.
@@ -23,19 +23,19 @@ var createBrowserify = function () {
 };
 
 var doBrowserify = function (bundle) {
-	return bundle.pipe(source('app.js'))
+	return bundle.pipe(source("app.js"))
 	.pipe(buffer())
-	.pipe(rename('app.js'))
+	.pipe(rename("app.js"))
 	.pipe(gulp.dest(output));
 };
 
-gulp.task('browserify', function () {
+gulp.task("browserify", function () {
 	var b = createBrowserify();
 	return doBrowserify(b.bundle());
 });
 
-gulp.task('copy_direct', function () {
-	gulp.src(['**/*.html', '**/*.png', 'favicon.ico', '**/*.css', 'font/*'], {
+gulp.task("copy_direct", function () {
+	gulp.src(["**/*.html", "**/*.png", "favicon.ico", "**/*.css", "font/*"], {
 		buffer : false
 	})
 	.pipe(gulp.dest(output));
@@ -43,8 +43,8 @@ gulp.task('copy_direct', function () {
 
 
 // The list of tasks to do by default
-var tasklist = ['copy_direct', 'browserify'];
+var tasklist = ["copy_direct", "browserify"];
 
-gulp.task('default',(function () {
+gulp.task("default",(function () {
     return tasklist;
 })());
--- src/website-react.js
+++ src/website-react.js
@@ -1,8 +1,8 @@
-var ReactDOM = require('react-dom');
-var React = require('react');
+var ReactDOM = require("react-dom");
+var React = require("react");
 
-var header = React.createElement(require('./react/header'));
-var footer = React.createElement(require('./react/footer'));
+var header = React.createElement(require("./react/header"));
+var footer = React.createElement(require("./react/footer"));
 
-ReactDOM.render(header, document.getElementById('header-wrapper'));
-ReactDOM.render(footer, document.getElementById('footer-wrapper'));
+ReactDOM.render(header, document.getElementById("header-wrapper"));
+ReactDOM.render(footer, document.getElementById("footer-wrapper"));
