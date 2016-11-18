afae2f6864dcaebce81a8f243c674f07890a4996
Mark Repka <mark@repkam09.com>
2016-11-18 09:20:07 -0500

    Add new page for api endpoints list

---
+++ src/react/apilist.jsx
@@ -0,0 +1,48 @@
+var React = require('react');
+
+module.exports = React.createClass({
+    getInitialState: function () {
+        return { apilist: [] };
+    },
+
+    componentDidMount: function () {
+        let that = this;
+        getreq("https://api.repkam09.com/api/about").then((response) => {
+            var apilist = JSON.parse(response);
+            that.setState({ apilist });
+        });
+    },
+
+    render: function () {
+        if (this.state.apilist.length > 0) {
+            var list = this.state.apilist.map((current) => {
+                return (
+                    <tr key={current.path}>
+                        <td>{current.method}</td>
+                        <td>{current.path}</td>
+                    </tr>
+                );
+            });
+
+            return (
+                <div className="api-info">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>Method</th>
+                                <th>Path</th>
+                            </tr>
+                            {list}
+                        </tbody>
+                    </table>
+                </div>
+            );
+        } else {
+            return (
+                <div className="api-info">
+                    <p>There are no active endpoints </p>
+                </div>
+            );
+        }
+    }
+});
--- src/website-react.js
+++ src/website-react.js
@@ -4,7 +4,17 @@ var React = require("react");
 var header = React.createElement(require("./react/header"));
 var footer = React.createElement(require("./react/footer"));
 var musicdata = React.createElement(require("./react/musicdata"));
+var apilist = React.createElement(require("./react/apilist"));
 
+// These should always exist on every page
 ReactDOM.render(header, document.getElementById("header-wrapper"));
 ReactDOM.render(footer, document.getElementById("footer-wrapper"));
-ReactDOM.render(musicdata, document.getElementById("music-data"));
+
+// These should only be loaded if we are on a page that uses them
+if (document.getElementById("react-api")) {
+    ReactDOM.render(apilist, document.getElementById("react-api"));
+}
+
+if (document.getElementById("music-data")) {
+    ReactDOM.render(musicdata, document.getElementById("music-data"));
+}
