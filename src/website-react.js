0e55688c5ae694410061d53000032162864a84df
Mark Repka <mark@repkam09.com>
2016-11-18 15:29:39 -0500

    Add the post/get request helpers to the bundle

--- src/website-react.js
+++ src/website-react.js
@@ -1,10 +1,73 @@
 var ReactDOM = require("react-dom");
 var React = require("react");
 
+
+/**
+ * Set up some helper functions that various pages use later
+ */
+window.postreq = function (url, body) {
+    return new Promise((resolve, reject) => {
+        var xmlhttp = new XMLHttpRequest();
+        var sendbody = body;
+        xmlhttp.open("POST", url, true);
+
+        let bodytype = typeof body;
+        switch (bodytype) {
+            case "object": {
+                xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+                sendbody = JSON.stringify(body);
+                break;
+            }
+
+            case "string": {
+                xmlhttp.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+                sendbody = body;
+                break;
+            }
+
+            default: {
+                xmlhttp.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+            }
+        }
+
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4) {
+                if (xmlhttp.status === 200) {
+                    resolve(xmlhttp.responseText);
+                } else {
+                    reject(xmlhttp.status);
+                }
+            }
+        }
+
+        xmlhttp.send(sendbody);
+    });
+}
+
+window.getreq = function (url) {
+    return new Promise((resolve, reject) => {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", url, true);
+
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4) {
+                if (xmlhttp.status === 200) {
+                    resolve(xmlhttp.responseText);
+                } else {
+                    reject(xmlhttp.status);
+                }
+            }
+        }
+        xmlhttp.send();
+    });
+}
+
+
 var header = React.createElement(require("./react/header"));
 var footer = React.createElement(require("./react/footer"));
 var musicdata = React.createElement(require("./react/musicdata"));
 var apilist = React.createElement(require("./react/apilist"));
+var loginpage = React.createElement(require("./react/login/loginpage"));
 
 // These should always exist on every page
 ReactDOM.render(header, document.getElementById("header-wrapper"));
@@ -17,4 +80,8 @@ if (document.getElementById("react-api")) {
 
 if (document.getElementById("music-data")) {
     ReactDOM.render(musicdata, document.getElementById("music-data"));
+}
+
+if (document.getElementById("login-page")) {
+    ReactDOM.render(loginpage, document.getElementById("login-page"));
 }
