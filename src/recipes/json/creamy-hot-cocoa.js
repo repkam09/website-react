ad396b05eaadb8658d9ca2443c3ff81f7c483430
Mark Repka <mark@repkam09.com>
2016-12-15 09:28:37 -0500

    Start of recipe page

---
+++ src/recipes/json/creamy-hot-cocoa.js
@@ -0,0 +1,37 @@
+var recipedata = {
+    name: "Creamy Hot Cocoa",
+    image: "http://images.media-allrecipes.com/userphotos/250x250/509474.jpg",
+    credits_name: "Jeanie Bean",
+    credits_url: "http://allrecipes.com/recipe/20211/creamy-hot-cocoa/print/",
+    description: "It's old fashioned, it's comforting, it makes the kitchen smell wonderful and it's good for the soul.",
+    ingredients: [
+        {
+            amount: "1/3 cup",
+            name: "unsweetened cocoa powder"
+        },
+        {
+            amount: "3/4 cup",
+            name: "white sugar"
+        },
+        {
+            amount: "1/3",
+            name: "cup boiling water"
+        },
+        {
+            amount: "3 1/2 cups",
+            name: "1% milk"
+        },
+        {
+            amount: "3/4 teaspoon",
+            name: "vanilla extract"
+        },
+        {
+            amount: "1/2 cup",
+            name: "half-and-half cream"
+        }
+
+    ],
+    directions: [
+        "Combine the cocoa, sugar and pinch of salt in a saucepan. Blend in the boiling water. Bring this mixture to an easy boil while you stir. Simmer and stir for about 2 minutes. Watch that it doesn't scorch. Stir in 3 1/2 cups of milk and heat until very hot, but do not boil! Remove from heat and add vanilla. Divide between 4 mugs. Add the cream to the mugs of cocoa to cool it to drinking temperature."
+    ]
+}
